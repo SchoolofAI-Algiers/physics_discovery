2f7e3017b117ab2d5dab14d84217b05bea3ae7eb
amine-ziad-ounnoughene <amineziadus@gmail.com>
2023-07-09 12:52:01 +0100

    save the generated data as csv

--- data_gen.py
+++ data_gen.py
@@ -1,7 +1,5 @@
 import numpy as np
-import json
-import os
-import uuid
+import pandas as pd
 size = 100
 mu_t, sigma_t = 0, 1
 t = np.random.normal(mu_t, sigma_t, size)
@@ -12,22 +10,16 @@ st = np.random.lognormal(mu_st, sigma_st, size)
 # the function that we generate the data with
 def f(t, st, fr):
     return st**2 * (fr * ((1- t)/st - np.exp(-t/st)))
-# create a folder to store all the timeseries
-if os.path.exists("data") == False:
-    os.mkdir("data")
 data = []
 for st_ in st:
     for fr_ in fr:
-        path = f"data/{uuid.uuid4()}.npy"
-        np.save(path, f(t, st_, fr_))
-        data.append(
-            {
-                "time_serie" : path,
-                "st" : st_,
-                "fr" : fr_
-            }
-        )
-print("data size : ", len(data))
-# save it as a json 
-with open("data.json", "w") as json_file:
-    json.dump({"data" : data}, json_file)
+        example = list(f(t, st_, fr_))
+        example.append(fr_)
+        example.append(st_)
+        data.append(example)
+data = np.array(data)
+colummns = [str(i) for i in range(size)]
+colummns.append("fr")
+colummns.append("st")
+df = pd.DataFrame(data,columns=colummns)
+df.to_csv("data.csv")
