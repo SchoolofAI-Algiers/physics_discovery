e695e1dbec97a46b6717f7a1ebf5b026e86b5943
amine-ziad-ounnoughene <amineziadus@gmail.com>
2023-07-22 21:25:14 +0100

    fixed some bugs

--- data_gen.py
+++ data_gen.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 size = 100
-mu_t, sigma_t = 0, 1
-t = np.random.normal(mu_t, sigma_t, size)
+t_max = 5
+t = np.linspace(0, t_max, size)
 mu_fr, sigma_fr = 0, 5
 fr = np.random.normal(mu_fr, sigma_fr, size)
 start_st, end_st = 0.01, 5
@@ -15,12 +15,18 @@ data = []
 for st_ in st:
     for fr_ in fr:
         example = list(f(t, st_, fr_))
+        t_pred = np.random.uniform(0, t_max)
+        pred = f(t_pred,st_,fr_)
         example.append(fr_)
         example.append(st_)
+        example.append(t_pred)
+        example.append(pred)
         data.append(example)
 data = np.array(data)
 colummns = [str(i) for i in range(size)]
 colummns.append("fr")
 colummns.append("st")
+colummns.append("t_pred")
+colummns.append("pred")
 df = pd.DataFrame(data,columns=colummns)
 df.to_csv("data.csv")
